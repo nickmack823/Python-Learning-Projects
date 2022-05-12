b3ebd323ebf456b82d7a22d2b10db40c75d5cd26
MacKinnon, Nicholas Stephen <nicmacki@iu.edu>
2022-05-11 21:37:07 -0400

    Exploring Selenium

--- Selenium/greek_scraping.py
+++ Selenium/greek_scraping.py
@@ -7,32 +7,43 @@ chrome_driver_path = "C:\Development\chromedriver.exe"
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-
-
 with open('greek_characters.txt') as file:
     entities = file.readlines()
+reversed_entities = entities[::-1]
 
 symbols = {}
-for entity in entities:
-    to_search = entity.replace('\n', '')
-    print(to_search)
-    # Searching
-    search = driver.find_element(by=By.NAME, value='search')
-    search.send_keys(to_search)
-    search.send_keys(Keys.ENTER)
-    result = driver.find_element(by=By.LINK_TEXT, value=to_search)
-    result.click()
 
-    labels = driver.find_elements(by=By.CLASS_NAME, value='infobox-label')
-    info = driver.find_elements(by=By.CLASS_NAME, value='infobox-data')
-    if len(labels) == 0:
-        continue
-    for n in range(len(labels)):
-        label = labels[n].text
-        if label == 'Symbol':
-            symbols[to_search] = info[n].text
-            break
 
-print(symbols)
+def scrape_symbols(entities):
+    for entity in entities:
+        to_search = entity.replace('\n', '')
+        print(to_search)
+        if to_search in symbols.keys():
+            print('continuing')
+            continue
+        symbols[to_search] = ''
+        # Searching
+        driver.get(f"https://en.wikipedia.org/wiki/{to_search}")
+
+        labels = driver.find_elements(by=By.CLASS_NAME, value='infobox-label')
+        info = driver.find_elements(by=By.CLASS_NAME, value='infobox-data')
+        if len(labels) == 0:
+            continue
+        for n in range(len(labels)):
+            label = labels[n].text
+            if label == 'Symbol':
+                symbols[to_search] = info[n].text
+                break
+
+
+def record_symbols(symbols):
+    with open('greek_god_symbols.txt', 'w') as f:
+        for god in symbols:
+            if symbols[god] != '':
+                f.write(f"{god}: {symbols[god]}\n")
+
+
+scrape_symbols(entities)
+record_symbols(symbols)
 driver.quit()
+
